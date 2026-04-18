b513b3525d5dfef7ba7b109b5545e57e17994d2c
koltainorbert <koltainorbert@users.noreply.github.com>
2026-04-18 22:36:46 +0200

    Auto_2026.04.18_22.36

--- wp-plugin/vadaszapro-core/frontend/templates/user/register-form.php
+++ wp-plugin/vadaszapro-core/frontend/templates/user/register-form.php
@@ -21,30 +21,63 @@ if ( is_user_logged_in() ) {
         <form method="post" class="va-register-form">
             <?php wp_nonce_field( 'va_register', 'va_register_nonce' ); ?>
             <input type="hidden" name="va_action" value="register">
+            <input type="hidden" name="reg_account_type" id="va-account-type" value="private">
 
             <div class="va-register-grid">
+                <div class="va-register-field va-register-field--full">
+                    <div class="va-account-switch" role="group" aria-label="Fiók típus választó">
+                        <span class="va-account-switch__label">Magánszemély</span>
+                        <label class="va-account-switch__toggle" for="va-account-type-switch">
+                            <input type="checkbox" id="va-account-type-switch" class="va-account-switch__input" aria-label="Céges fiók kapcsoló">
+                            <span class="va-account-switch__slider" aria-hidden="true"></span>
+                        </label>
+                        <span class="va-account-switch__label">Cég</span>
+                    </div>
+                </div>
+
                 <div class="va-form-group va-register-field">
                     <label>Keresztnév <span class="required">*</span></label>
-                    <input type="text" name="reg_firstname" class="va-input" required>
+                    <input type="text" name="reg_firstname" class="va-input" required data-typing="András|János|Péter">
                 </div>
                 <div class="va-form-group va-register-field">
                     <label>Vezetéknév</label>
-                    <input type="text" name="reg_lastname" class="va-input">
+                    <input type="text" name="reg_lastname" class="va-input" data-typing="Nagy|Kovács|Tóth">
                 </div>
 
                 <div class="va-form-group va-register-field">
                     <label>Felhasználónév <span class="required">*</span></label>
-                    <input type="text" name="reg_username" class="va-input" required autocomplete="username">
+                    <input type="text" name="reg_username" class="va-input" required autocomplete="username" data-typing="vadasz1988|golyospuska|trofea_user">
                 </div>
 
                 <div class="va-form-group va-register-field">
                     <label>E-mail cím <span class="required">*</span></label>
-                    <input type="email" name="reg_email" class="va-input" required autocomplete="email">
+                    <input type="email" name="reg_email" class="va-input" required autocomplete="email" data-typing="pelda@email.hu|info@cegem.hu">
                 </div>
 
                 <div class="va-form-group va-register-field">
                     <label>Telefonszám</label>
-                    <input type="tel" name="reg_phone" class="va-input" placeholder="+36 30 000 0000">
+                    <input type="tel" name="reg_phone" class="va-input" placeholder="+36 30 000 0000" data-typing="+36 30 123 4567|+36 70 765 4321">
+                </div>
+
+                <div class="va-register-company va-register-field va-register-field--full" aria-hidden="true">
+                    <div class="va-register-company__grid">
+                        <div class="va-form-group va-register-field">
+                            <label>Cégnév <span class="required">*</span></label>
+                            <input type="text" name="reg_company_name" class="va-input" data-company-field="1" disabled data-typing="Minta Vadász Kft.|Erdővad Bt.">
+                        </div>
+                        <div class="va-form-group va-register-field">
+                            <label>Adószám <span class="required">*</span></label>
+                            <input type="text" name="reg_company_tax" class="va-input" data-company-field="1" disabled data-typing="12345678-2-42|98765432-1-13">
+                        </div>
+                        <div class="va-form-group va-register-field">
+                            <label>Székhely <span class="required">*</span></label>
+                            <input type="text" name="reg_company_seat" class="va-input" data-company-field="1" disabled data-typing="1123 Budapest, Minta utca 10.|6720 Szeged, Fő tér 3.">
+                        </div>
+                        <div class="va-form-group va-register-field">
+                            <label>Személynév <span class="required">*</span></label>
+                            <input type="text" name="reg_contact_name" class="va-input" data-company-field="1" disabled data-typing="Kiss Gábor|Szabó Anna">
+                        </div>
+                    </div>
                 </div>
 
                 <div class="va-form-group va-register-field">
