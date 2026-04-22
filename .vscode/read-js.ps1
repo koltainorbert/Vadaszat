89e30ca2b99a0f122af03c86defb0c5726263379
koltainorbert <koltainorbert@users.noreply.github.com>
2026-04-22 13:56:04 +0200

    Auto_2026.04.22_13.56

---
+++ .vscode/read-js.ps1
@@ -0,0 +1,4 @@
+﻿$b=[System.IO.File]::ReadAllBytes("D:\Vadaszat2026\wp-plugin\vadaszapro-core\admin\admin.js")
+$c=[System.Text.Encoding]::Unicode.GetString($b)
+$i=$c.IndexOf("vaInitColorPickers")
+Write-Output $c.Substring($i,2000)
