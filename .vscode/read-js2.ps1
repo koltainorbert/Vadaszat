27d0f5344489c2cb5d2cff44334012d845610163
koltainorbert <koltainorbert@users.noreply.github.com>
2026-04-22 13:58:05 +0200

    Auto_2026.04.22_13.58

---
+++ .vscode/read-js2.ps1
@@ -0,0 +1,6 @@
+﻿@
+$b=[System.IO.File]::ReadAllBytes("D:\Vadaszat2026\wp-plugin\vadaszapro-core\admin\admin.js")
+$c=[System.Text.Encoding]::Unicode.GetString($b)
+$i=$c.IndexOf("syncUI")
+Write-Output $c.Substring($i, 1200)
+@"
